a50736cd3c6d5f2a987cbe874b9227fc28e8fd10
Tom300591 <thomas.laine@hotmail.fr>
2025-05-17 11:49:50 +0200

    log

--- cinephoria-backend/src/database/postgre.js
+++ cinephoria-backend/src/database/postgre.js
@@ -30,4 +30,6 @@ sequelize.authenticate()
   .then(() => console.log('Connexion PostgreSQL OK'))
   .catch(err => console.error('Erreur de connexion PostgreSQL :', err))
 
+console.log('DATABESE_URL ===>', process.env.DATABASE_URL)
+
 module.exports = sequelize
